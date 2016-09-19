03b57f10caecac14eef9f5a8831065952e6f9c25
Lyuha <beingfunnylife@gmail.com>
2016-09-19 18:14:25 +0900

    remove unused braces

--- src/main.rs
+++ src/main.rs
@@ -46,51 +46,51 @@ fn main() {
                     }
                 }
             }
-            {
-                let source_bytes = source.as_bytes();
-                let mut program_counter: usize = 0;
-                let mut vm = BFMemory::new();
 
-                while program_counter < source_bytes.len() {
-                    match source_bytes[program_counter] as char {
-                        '+' => vm.increase(),
-                        '-' => vm.decrease(),
-                        '<' => vm.move_left(),
-                        '>' => vm.move_right(),
-                        '[' => {
-                            if vm.get_value() == 0 {
-                                if let Some(&v) = jump_positions.get(&program_counter) {
-                                    program_counter = v;
-                                }
-                            }
-                        }
-                        ']' => {
-                            if vm.get_value() != 0 {
-                                if let Some(&v) = jump_positions.get(&program_counter) {
-                                    program_counter = v;
-                                }
-                            }
-                        }
-                        '.' => {
-                            print!("{}", vm.get_value() as char);
-                        }
-                        ',' => {
-                            let mut buf = [0];
-                            let mut handle = std::io::stdin().take(1);
+            let source_bytes = source.as_bytes();
+            let mut program_counter: usize = 0;
+            let mut vm = BFMemory::new();
 
-                            if handle.read(&mut buf).is_ok() {
-                                vm.set_value(buf[0])
+            while program_counter < source_bytes.len() {
+                match source_bytes[program_counter] as char {
+                    '+' => vm.increase(),
+                    '-' => vm.decrease(),
+                    '<' => vm.move_left(),
+                    '>' => vm.move_right(),
+                    '[' => {
+                        if vm.get_value() == 0 {
+                            if let Some(&v) = jump_positions.get(&program_counter) {
+                                program_counter = v;
                             }
                         }
-                        _ => (),
                     }
-                    program_counter += 1;
+                    ']' => {
+                        if vm.get_value() != 0 {
+                            if let Some(&v) = jump_positions.get(&program_counter) {
+                                program_counter = v;
+                            }
+                        }
+                    }
+                    '.' => {
+                        print!("{}", vm.get_value() as char);
+                    }
+                    ',' => {
+                        let mut buf = [0];
+                        let mut handle = std::io::stdin().take(1);
+
+                        if handle.read(&mut buf).is_ok() {
+                            vm.set_value(buf[0])
+                        }
+                    }
+                    _ => (),
                 }
+                program_counter += 1;
             }
-        } else {
-            if let Err(e) = writeln!(&mut std::io::stderr(), "File '{}' don't exist", arg) {
-                println!("\nWriting Error: {}", e);
-            }
+
+        } else if let Err(e) = writeln!(&mut std::io::stderr(), "File '{}' don't exist", arg) {
+            println!("\nWriting Error: {}", e);
+
         }
+
     }
 }
